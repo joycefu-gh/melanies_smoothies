0b3398d046efdb8179097f74728eac75e0167681
joycefu-gh <joyce.fu.ut.i@gmail.com>
2025-04-22 21:30:30 +0000

    fixed search_on col

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
-        fruitycice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        fruitycice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data=fruitycice_response.json(), use_container_width=True)
 
 

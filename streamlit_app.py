bc7383ced555d9fc968f6570cc16ad6f02f23460
joycefu-gh <joyce.fu.ut.i@gmail.com>
2025-04-22 19:55:10 +0000

    put import requests to the top

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
+import requests
+
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
 
 helpful_links = [
     "https://docs.streamlit.io",
